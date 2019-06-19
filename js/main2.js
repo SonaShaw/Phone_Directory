ee84a9a53fff04c09f135ac8b0c1dea6a42a9f86
Sona Shaw <51262648+SonaShaw@users.noreply.github.com>
2019-06-19 17:30:35 +0530

    JavaScript methods
    main1.java for index1.html.
    main2.java  for index2.html.

---
+++ js/main2.js
@@ -0,0 +1,11 @@
+
+
+function disp(){
+	var name = document.getElementById("nameInput").value;
+	var number = document.getElementById("numberInput").value;
+
+	document.getElementById("nameId").innerHTML = name;
+	document.getElementById("numberId").innerHTML = number;
+}
+	
+	
